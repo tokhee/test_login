677fb490865dc8cacfef787d1850de6d7f195e3f
jianha <hazianist@icloud.com>
2025-01-17 13:54:51 +0900

    Add phone_number field and webrick gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_17_034637) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_17_045427) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -19,6 +19,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_17_034637) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "phone_number"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 end
